7b8bfee57d015b8f787be894fddf3a76322a7676
Ioana Marinescu <sh ./setup.shioana.marinescu@stu.ocsb.ca>
2022-09-16 02:00:06 +0000

    hello world with proper style

---
+++ hello_world_style.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Copyright (c) 2022 Ioana Marinescu All rights reserved.
+# Created By: Ioana Marinescu
+#
+# Date: Sept. 14
+# prints with propers styling
+
+
+def main():
+    print("Hello world! (with proper style!)")
+
+
+if __name__ == "__main__":
+    main()
